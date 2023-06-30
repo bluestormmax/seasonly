58929398caea05d9602ee058cc750ee5564cc2e2
Max MacMillan <max@treat.xyz>
2023-06-30 16:33:04 -0400

    Quick state test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
-import { Typography } from "@mui/material";
+import { useState } from "react";
+import { Typography, Button } from "@mui/material";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="app wrapper">
       <Typography className="heading welcome" variant="h1" component="h1">
         hello!
       </Typography>
+      <Button variant="contained" onClick={handleClick}>
+        Count: {count}
+      </Button>
     </div>
   );
 }
