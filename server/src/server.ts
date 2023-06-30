45dcd670769278eb2d6d26db10e21873448e2f55
bluestormcreative <max@bluestormcreative.com>
2023-06-29 23:26:38 -0400

    Use .env and mongoose

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,19 @@
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
-app.listen(port, () => {
-  console.log("server running on port: " + port);
-});
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING!)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => {
+      console.log("Server running on port: " + port);
+    });
+  })
+  .catch(console.error);
