e499c83b80c302f7be1ed43c9f9aa7c256a5b573
bluestormcreative <max@bluestormcreative.com>
2023-06-29 23:35:48 -0400

    Use envalid to sanitize and validate env vars

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,16 @@
 import "dotenv/config";
+import env from "./util/validateEnv";
 import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = process.env.PORT;
+const port = env.PORT;
 
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
 mongoose
-  .connect(process.env.MONGO_CONNECTION_STRING!)
+  .connect(env.MONGO_CONNECTION_STRING)
   .then(() => {
     console.log("Mongoose connected");
     app.listen(port, () => {
