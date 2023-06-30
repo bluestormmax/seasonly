82b4a0140b7c6e86f69eb4a27bc75ff1db8c2226
bluestormcreative <max@bluestormcreative.com>
2023-06-29 23:40:29 -0400

    Separate concerns for future testing - app endpoint vs db connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,8 @@
-import "dotenv/config";
+import app from "./app";
 import env from "./util/validateEnv";
 import mongoose from "mongoose";
-import express from "express";
-const app = express();
-const port = env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
-});
+const port = env.PORT;
 
 mongoose
   .connect(env.MONGO_CONNECTION_STRING)
