88cc9a6abe03fdfbdd48ac22c465105016b12d13
Max MacMillan <max@treat.xyz>
2023-06-30 13:42:05 -0400

    Add error handling middleware to express endpoints

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,24 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import ShoppingListModel from "./models/shoppingList";
 
 const app = express();
 
-app.get("/", async (req, res) => {
-  const shoppingLists = await ShoppingListModel.find().exec();
-  res.status(200).json(shoppingLists);
+app.get("/", async (req, res, next) => {
+  try {
+    const shoppingLists = await ShoppingListModel.find().exec();
+    res.status(200).json(shoppingLists);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Express error handler.
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  let errorMessage = "An unknown error occurred";
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
