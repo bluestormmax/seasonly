f3c55b7b20a3c9942e139f3a31e2c7b149844dae
Max MacMillan <max@treat.xyz>
2023-07-03 15:41:24 -0400

    Set username and pw to be unique in schema

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -2,8 +2,8 @@ import { InferSchemaType, model, Schema } from "mongoose";
 
 const userSchema = new Schema(
   {
-    username: { type: String, required: true },
-    email: { type: String, required: true, select: false },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, select: false },
     password: { type: String, required: true, select: false },
   },
   {
