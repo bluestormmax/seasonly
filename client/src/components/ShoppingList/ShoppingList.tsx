a56ba3f023e098a799fa2ea6a26271b71874abf1
Max MacMillan <max@treat.xyz>
2023-07-03 14:46:23 -0400

    Set up delete handler for lists

--- client/src/components/ShoppingList/ShoppingList.tsx
+++ client/src/components/ShoppingList/ShoppingList.tsx
@@ -14,9 +14,14 @@ import styles from "./ShoppingList.module.css";
 type ShoppingListProps = {
   shoppingList: ShoppingListModel;
   className?: string;
+  onDeleteListClicked: (shoppingList: ShoppingListModel) => void;
 };
 
-const ShoppingList = ({ shoppingList, className }: ShoppingListProps) => {
+const ShoppingList = ({
+  shoppingList,
+  className,
+  onDeleteListClicked,
+}: ShoppingListProps) => {
   const { title, list, createdAt, updatedAt } = shoppingList;
   const timeStamp: string = updatedAt ? updatedAt : createdAt;
   const date: string = formatDate(timeStamp);
@@ -36,7 +41,12 @@ const ShoppingList = ({ shoppingList, className }: ShoppingListProps) => {
         <Typography variant="body2" component="span">
           Last updated at: {date}
         </Typography>
-        <IconButton>
+        <IconButton
+          onClick={(event) => {
+            onDeleteListClicked(shoppingList);
+            event.stopPropagation();
+          }}
+        >
           <DeleteOutline />
         </IconButton>
       </Box>
