153447d3514edb13d6ac473f97178e17dd7a7eff
Max MacMillan <max@treat.xyz>
2023-07-03 21:34:03 -0400

    Start to tie lists to users - add user id to list schema

--- server/src/models/shoppingList.ts
+++ server/src/models/shoppingList.ts
@@ -2,6 +2,7 @@ import { InferSchemaType, model, Schema } from "mongoose";
 
 const shoppingListSchema = new Schema(
   {
+    userId: { type: Schema.Types.ObjectId, required: true },
     title: { type: String, required: true },
     list: { type: String, required: true },
   },
