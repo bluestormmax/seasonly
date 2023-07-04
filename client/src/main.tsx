de1cc01c1706ecf723b48f6e597d152e23842c89
Max MacMillan <max@treat.xyz>
2023-07-03 22:06:50 -0400

    Toss in palette and theme provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import App from "./App.tsx";
 import "./global.css";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#00796b",
+    },
+    secondary: {
+      main: "#ff5722",
+    },
+    error: {
+      main: "#d32f2f",
+    },
+    success: {
+      main: "#1e88e5",
+    },
+    info: {
+      main: "#76ff03",
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
