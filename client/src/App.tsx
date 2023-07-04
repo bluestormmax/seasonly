d024344b0e8b5f335db7330a0ada1ab9a5fcbe1d
Max MacMillan <max@treat.xyz>
2023-07-03 21:29:20 -0400

    Wire up login and signup buttons and lists page views

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,60 @@
+import { useState, useEffect } from "react";
+import { Box } from "@mui/material";
+import { User as UserModel } from "@models/user";
+import * as UserApi from "@api/user.api";
 import { SignUpDialog, LoginDialog, NavBar } from "./components";
+import {
+  ListsPageLoggedInView,
+  ListsPageLoggedOutView,
+} from "./pages/NotesPage";
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState<UserModel | null>(null);
+  const [showSignUpDialog, setShowSignUpDialog] = useState(false);
+  const [showLoginDialog, setShowLoginDialog] = useState(false);
+
+  useEffect(() => {
+    async function fetchLoggedInUser() {
+      try {
+        const user = await UserApi.getLoggedInUser();
+        setLoggedInUser(user);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchLoggedInUser();
+  });
+
   return (
     <div className="app wrapper">
       <NavBar
-        loggedInUser={null}
-        onLoginClicked={() => {}}
-        onSignUpClicked={() => {}}
-        onLogOutSuccess={() => {}}
+        loggedInUser={loggedInUser}
+        onLoginClicked={() => setShowLoginDialog(true)}
+        onSignUpClicked={() => setShowSignUpDialog(true)}
+        onLogOutSuccess={() => setLoggedInUser(null)}
       />
+      <Box className="main" mt={2}>
+        {loggedInUser ? <ListsPageLoggedInView /> : <ListsPageLoggedOutView />}
+      </Box>
 
-      {false && (
-        <SignUpDialog onDismiss={() => {}} onSignupSuccess={() => {}} />
-      )}
-      {true && <LoginDialog onDismiss={() => {}} onLoginSuccess={() => {}} />}
+      {showSignUpDialog ? (
+        <SignUpDialog
+          onDismiss={() => setShowSignUpDialog(false)}
+          onSignupSuccess={(user) => {
+            setLoggedInUser(user);
+            setShowSignUpDialog(false);
+          }}
+        />
+      ) : null}
+      {showLoginDialog ? (
+        <LoginDialog
+          onDismiss={() => setShowLoginDialog(false)}
+          onLoginSuccess={(user) => {
+            setLoggedInUser(user);
+            setShowLoginDialog(false);
+          }}
+        />
+      ) : null}
     </div>
   );
 }
