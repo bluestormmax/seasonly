f7a21b3f0916680c86105039d92ceb6f19319e6d
Max MacMillan <max@treat.xyz>
2023-07-05 21:10:15 -0400

    Add single user get route

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.get("/", requiresAuth, UserController.getAuthenticatedUser);
 
+router.get("/:userId", requiresAuth, UserController.getAuthenticatedUser);
+
 router.post("/signup", UserController.signUp);
 
 router.post("/login", UserController.login);
