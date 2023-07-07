83eb71f4dbe338ca6e88e6497d35a51eeeb2e43d
Max MacMillan <max@treat.xyz>
2023-07-07 12:54:34 -0400

    Add zip to user model

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -6,6 +6,7 @@ const userSchema = new Schema(
     email: { type: String, required: true, unique: true, select: false },
     password: { type: String, required: true, select: false },
     state: { type: String },
+    zip: { type: String },
     zone: {
       zone: { type: String },
       coordinates: { lat: String, lon: String },
