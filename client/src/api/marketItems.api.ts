4c12bdf0d58ea8e951a4fa8e13ec48874e80db61
Max MacMillan <max@treat.xyz>
2023-07-07 17:21:06 -0400

    Add client api for seasonal endpoint

--- client/src/api/marketItems.api.ts
+++ client/src/api/marketItems.api.ts
@@ -11,3 +11,24 @@ export async function fetchAllMarketItems(): Promise<MarketItem[]> {
   });
   return response.json();
 }
+
+export interface SeasonalData {
+  zone?: string;
+  month?: string;
+}
+
+export async function fetchInSeasonMarketItems(
+  data: SeasonalData
+): Promise<MarketItem[]> {
+  const response = await fetchData(
+    `/api/marketItems/seasonal/${data.zone}/${data.month}`,
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "GET",
+    }
+  );
+  return response.json();
+}
