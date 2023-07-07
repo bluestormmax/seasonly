4b97ff3f3495f13788e3d2069d5f3011f2f3622b
Max MacMillan <max@treat.xyz>
2023-07-07 16:34:47 -0400

    Remove unused vars for upserting, create search $project correctly

--- server/src/controllers/marketItems.ts
+++ server/src/controllers/marketItems.ts
@@ -1,19 +1,11 @@
 import { RequestHandler } from "express";
 import createHttpError from "http-errors";
-import env from "../util/validateEnv";
+import {
+  MONGODB_DATABASE,
+  MONGODB_COLLECTION,
+  mongoClient,
+} from "../util/dbClient";
 import MarketItemModel from "../models/marketItems";
-
-const ATLAS_API_BASE_URL = "https://cloud.mongodb.com/api/atlas/v1.0";
-const ATLAS_PROJECT_ID = env.MONGODB_ATLAS_PROJECT_ID;
-const ATLAS_CLUSTER_NAME = env.MONGODB_ATLAS_CLUSTER;
-const ATLAS_CLUSTER_API_URL = `${ATLAS_API_BASE_URL}/groups/${ATLAS_PROJECT_ID}/clusters/${ATLAS_CLUSTER_NAME}`;
-const ATLAS_SEARCH_INDEX_API_URL = `${ATLAS_CLUSTER_API_URL}/fts/indexes`;
-
-const ATLAS_API_PUBLIC_KEY = env.MONGODB_ATLAS_PUBLIC_KEY;
-const ATLAS_API_PRIVATE_KEY = env.MONGODB_ATLAS_PRIVATE_KEY;
-const DIGEST_AUTH = `${ATLAS_API_PUBLIC_KEY}:${ATLAS_API_PRIVATE_KEY}`;
-
-const IN_SEASON_SEARCH_INDEX_NAME = "searchMarketItems";
 
 // Get all market items.
 export const getMarketItems: RequestHandler = async (req, res, next) => {
@@ -65,33 +57,31 @@ export const getSeasonalMarketItems: RequestHandler = async (
   res,
   next
 ) => {
-  const zone = "6a";
-  const month = "July";
+  const zone = req.body.zone;
+  const month = req.body.month;
   const zoneDigit = zone.replace(/[^0-9]/g, "");
-  const zoneToFind = new RegExp(zoneDigit + "$");
-  const query = { zone: zoneToFind };
 
-  const db = mongoClient.db("tutorial");
-  const collection = db.collection<User>(MONGODB_COLLECTION);
+  const db = mongoClient.db(MONGODB_DATABASE);
+  const collection = db.collection(MONGODB_COLLECTION);
 
   const pipeline = [];
 
   pipeline.push({
     $search: {
-      index: IN_SEASON_SEARCH_INDEX_NAME,
+      index: "searchMarketItems",
       compound: {
         must: [
           {
             text: {
-              query: zone,
-              path: "zones",
+              query: zoneDigit,
+              path: "zones.zone",
               fuzzy: {},
             },
           },
           {
             text: {
               query: month,
-              path: "harvest_dates",
+              path: "zones.harvest_dates",
             },
           },
         ],
@@ -99,8 +89,18 @@ export const getSeasonalMarketItems: RequestHandler = async (
     },
   });
 
+  pipeline.push({
+    $project: {
+      _id: 1,
+      itemType: 1,
+      displayName: 1,
+      name: 1,
+    },
+  });
+
   try {
-    const marketItems = await MarketItemModel.find(query).exec();
+    const result = await collection.aggregate(pipeline);
+    const marketItems = await result.toArray();
     if (marketItems.length === 0) {
       throw createHttpError(404, "No in-season items found");
     }
