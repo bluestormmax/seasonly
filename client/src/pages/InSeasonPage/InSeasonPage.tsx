4c53fbd049b7d5e8ce7146610fb66a3c43db97d2
Max MacMillan <max@treat.xyz>
2023-07-09 13:58:21 -0400

    Set up fetch from pexels, returns but can’t render

--- client/src/pages/InSeasonPage/InSeasonPage.tsx
+++ client/src/pages/InSeasonPage/InSeasonPage.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { createClient } from "pexels";
 import { Box, Typography, CircularProgress, Link } from "@mui/material";
 import { User as UserModel } from "@models/user";
 import { MarketItem as MarketItemModel } from "@models/marketItem";
@@ -19,6 +20,9 @@ const InSeasonPage = ({ loggedInUser }: InSeasonPageProps) => {
   const [showItemsLoadingError, setShowItemsLoadingError] = useState(false);
   const [marketItems, setMarketItems] = useState<MarketItemModel[]>([]);
   const [month, setMonth] = useState<string>(getMonthName());
+  const pexelsClient = createClient(
+    "3QhQS8c66nweCU5fbJq5qouyYBQaC3vi0URYkWdyYuXHPAPvoGlzYnTH"
+  );
 
   const zoneText = `Your growing zone is: ${zone?.zone} in ${usState}`;
 
@@ -46,7 +50,25 @@ const InSeasonPage = ({ loggedInUser }: InSeasonPageProps) => {
           setItemsLoading(true);
           const initialMarketItems =
             await MarketItemsApi.fetchInSeasonMarketItems(seasonalData);
-          setMarketItems(initialMarketItems);
+
+          console.log("INITIAL: ", initialMarketItems);
+          const marketItemsWithImageUrls = initialMarketItems.map((item) => {
+            if (item.name === "nectarines") {
+              try {
+                const res = pexelsClient.photos.show({ id: item.image });
+                res.then((photo) => {
+                  console.log("PHOTO: ", photo);
+                  item.imageUrl = photo.src.medium;
+                  return item;
+                });
+              } catch (error) {
+                console.log(error);
+              }
+            }
+            return item;
+          });
+          console.log("IMAGES: ", marketItemsWithImageUrls);
+          setMarketItems(marketItemsWithImageUrls);
         } catch (error) {
           console.error(error);
           setShowItemsLoadingError(true);
@@ -82,8 +104,7 @@ const InSeasonPage = ({ loggedInUser }: InSeasonPageProps) => {
             {marketItems.map((item) => (
               <Box key={item.name}>
                 <h3>{item.displayName}</h3>
-                {item.image}
-                <img src={item.image} alt={item.name} />
+                <img src="https://images.pexels.com/photos/17406435/pexels-photo-17406435.jpeg?auto=compress&cs=tinysrgb&h=350" />
               </Box>
             ))}
           </Box>
