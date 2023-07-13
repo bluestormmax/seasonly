8544db5ebf5c98d96328990ad78fcc368701f1f0
Max MacMillan <max@treat.xyz>
2023-07-12 21:49:47 -0400

    Add check in list helper

---
+++ client/src/utils/checkIfInList.ts
@@ -0,0 +1,8 @@
+import { ListItemModel } from "@/models/shoppingList";
+
+export function checkIfInList(
+  arr: ListItemModel[],
+  item: ListItemModel
+): boolean {
+  return arr.some((el) => el["name"] === item["name"]);
+}
