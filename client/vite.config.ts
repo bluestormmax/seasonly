ad4780fe884122157ecbd7466e6c04c9e16a5c3c
Max MacMillan <max@treat.xyz>
2023-07-13 14:52:51 -0400

    Use env in vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,33 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import env from "./util/validateEnv";
 import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-      "@models": path.resolve(__dirname, "./src/models"),
-      "@api": path.resolve(__dirname, "./src/api"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@utils": path.resolve(__dirname, "./src/utils"),
+        "@models": path.resolve(__dirname, "./src/models"),
+        "@api": path.resolve(__dirname, "./src/api"),
+      },
     },
-  },
-  plugins: [
-    react(),
-    ViteWebfontDownload([
-      "https://fonts.googleapis.com/css?family=Slabo+27px",
-      "https://fonts.googleapis.com/css?family=Lato",
-    ]),
-  ],
-  server: {
-    proxy: {
-        '/api': {
-          target: env.APP_BASE_URL,
+    plugins: [
+      react(),
+      ViteWebfontDownload([
+        "https://fonts.googleapis.com/css?family=Slabo+27px",
+        "https://fonts.googleapis.com/css?family=Lato",
+      ]),
+    ],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL,
           changeOrigin: true,
           secure: false,
         },
+      },
+    },
+  };
 });
