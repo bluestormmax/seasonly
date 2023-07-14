2b9d216e2e4e900c3a08d8fd03b1774963094f92
Max MacMillan <max@treat.xyz>
2023-07-13 21:24:15 -0400

    Revert server file

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,39 +4,6 @@ import mongoose from "mongoose";
 
 const port = env.PORT;
 
-<<<<<<< HEAD
-// mongoose
-//   .connect(env.MONGO_CONNECTION_STRING)
-//   .then(() => {
-//     console.log("Mongoose connected");
-//     app.listen(port, () => {
-//       console.log("Server running on port: " + port);
-//     });
-//   })
-//   .catch(console.error);
-
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(env.MONGO_CONNECTION_STRING);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-};
-
-// Routes
-app.all("*", (req, res) => {
-  res.json({ "every thing": "is awesome" });
-});
-
-// Connect to the database before listening
-connectDB().then(() => {
-  app.listen(env.PORT, () => {
-    console.log("listening for requests");
-  });
-});
-=======
 mongoose
   .connect(env.MONGO_CONNECTION_STRING)
   .then(() => {
@@ -46,4 +13,3 @@ mongoose
     });
   })
   .catch(console.error);
->>>>>>> parent of 3f1c0a5 (Rename server file to try to kick vercel)
