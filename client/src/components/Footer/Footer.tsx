8870c4be254f5e9c97c954ec4bd33cbfb4817429
Max MacMillan <max@bluestormcreative.com>
2024-12-06 01:16:39 -0500

    Use svg in footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,14 +1,33 @@
-import { Box, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Box, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
-  <Box className="footer" component="footer" px={2}>
-    <Typography variant="caption" component="span">
-      &copy;{new Date().getFullYear()}{" "}
-      <Link to="https://bluestormcreative.com" target="blank">
-        blue storm creative
-      </Link>
-    </Typography>
+  <Box
+    className='footer'
+    component='footer'
+    sx={{ px: 2, height: '100%', display: 'flex', flexDirection: 'col' }}
+  >
+    <Box
+      component='div'
+      sx={{
+        color: 'white',
+        width: '100%',
+        alignSelf: 'flex-end',
+        backgroundColor: '#376E66',
+      }}
+    >
+      <Typography
+        variant='caption'
+        component='div'
+        align='center'
+        sx={{ position: 'relative', zIndex: 10 }}
+      >
+        &copy;{new Date().getFullYear()}{' '}
+        <Link to='https://bluestormcreative.com' target='blank'>
+          blue storm creative
+        </Link>
+      </Typography>
+    </Box>
   </Box>
 );
 
