98d3dc0634a876d1f6384894a8fa0588a56a1ebc
Max MacMillan <max@bluestormcreative.com>
2024-12-10 23:54:34 -0500

    Add some fruit is a sort of logo image

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -4,10 +4,15 @@ import {
   Typography,
   Toolbar,
   useMediaQuery,
-} from "@mui/material";
-import { Menu } from "@mui/icons-material";
-import { Link } from "react-router-dom";
-import { LoginWrapper } from "@/components";
+  Box,
+} from '@mui/material';
+import { Menu } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { LoginWrapper } from '@/components';
+import Pomegranate from '@assets/pomegranate.svg?react';
+import Broccoli from '@assets/broccoli.svg?react';
+import Eggplant from '@assets/eggplant.svg?react';
+import Apricot from '@assets/apricot.svg?react';
 
 // Null if user not logged in, but we don't want an optional prop here
 type NavBarProps = {
@@ -23,27 +28,41 @@ const NavBar = ({
   onLogOutSuccess,
   onMenuIconClicked,
 }: NavBarProps) => {
-  const matches = useMediaQuery("(min-width:600px)");
+  const matches = useMediaQuery('(min-width:600px)');
 
   return (
-    <AppBar position="static">
+    <AppBar position='static'>
       <Toolbar>
         <IconButton
-          size="large"
-          color="inherit"
-          aria-label="menu"
+          size='large'
+          color='inherit'
+          aria-label='menu'
           onClick={onMenuIconClicked}
         >
           <Menu />
         </IconButton>
         <Typography
-          variant="h6"
+          variant='h6'
           component={Link}
-          to="/"
-          className="fancy_text logo"
+          to='/'
+          className='fancy_text logo'
         >
           Seasonly
         </Typography>
+        <Box
+          sx={{
+            ml: '8px',
+            display: 'flex',
+            gap: '8px',
+            svg: { width: '20px', height: 'auto' },
+          }}
+        >
+          <Apricot />
+          <Pomegranate />
+          <Broccoli />
+          <Eggplant />
+        </Box>
+
         {matches ? (
           <LoginWrapper
             onSignUpClicked={onSignUpClicked}
